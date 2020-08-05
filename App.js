7fb0078460345f18f505540ec7c4fa42fd2f6313
GG <guie.a.g.r@gmail.com>
2020-08-05 14:45:29 -0700

    Inicial Commit
    React Native App, Calculator

---
+++ App.js
@@ -0,0 +1,184 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      resultText: '',
+      calculationText: '',
+    };
+
+    this.operations = ['DEL', '+', '-', '*', '/'];
+  }
+
+  calculateResult() {
+    const text = this.state.resultText;
+    this.setState({
+      calculationText: eval(text),
+    });
+  }
+
+  validate() {
+    const text = this.state.resultText;
+    switch (text.slice(-1)) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '.':
+        return false;
+    }
+    return true;
+  }
+
+  buttonPressed(text) {
+    if (text == '=') {
+      return this.validate() && this.calculateResult();
+    }
+
+    this.setState({
+      resultText: this.state.resultText + text,
+    });
+  }
+
+  operate(operation) {
+    switch (operation) {
+      case 'DEL':
+        let text = this.state.resultText.split('');
+        text.pop();
+        this.setState({
+          resultText: text.join(''),
+        });
+        break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        const lastChar = this.state.resultText.split('').pop();
+        if (this.operations.indexOf(lastChar) > 0) return;
+        if (this.state.text == '') return;
+        this.setState({
+          resultText: this.state.resultText + operation,
+        });
+    }
+  }
+
+  render() {
+    let rows = [];
+    let nums = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      ['.', '0', '='],
+    ];
+    for (let i = 0; i < 4; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(
+          <TouchableOpacity
+            key={nums[i][j]}
+            onPress={() => this.buttonPressed(nums[i][j])}
+            style={styles.btn}>
+            <Text style={styles.btntext}>{nums[i][j]}</Text>
+          </TouchableOpacity>,
+        );
+      }
+      rows.push(
+        <View key={i} style={styles.row}>
+          {row}
+        </View>,
+      );
+    }
+
+    let ops = [];
+
+    for (let i = 0; i < this.operations.length; i++) {
+      ops.push(
+        <TouchableOpacity
+          key={i}
+          onPress={() => this.operate(this.operations[i])}
+          style={styles.btn}>
+          <Text style={[styles.btntext, styles.white]}>
+            {this.operations[i]}
+          </Text>
+        </TouchableOpacity>,
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>{this.state.resultText}</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>
+            {this.state.calculationText}
+          </Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>{rows}</View>
+          <View style={styles.operations}>{ops}</View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  result: {
+    flex: 2,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  resultText: {
+    fontSize: 30,
+    color: 'black',
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  calculationText: {
+    fontSize: 24,
+    color: 'black',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: 'row',
+  },
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+  btntext: {
+    fontSize: 35,
+    color: 'white',
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: '#434343',
+  },
+  operations: {
+    flex: 1,
+    justifyContent: 'space-around',
+    backgroundColor: '#636363',
+  },
+  white: {
+    color: 'white',
+  },
+});
